e897c06c8dba0375a6c968cc638a7ad47dadfb94
Lou K <84414167+Loustoc@users.noreply.github.com>
2023-04-17 14:40:22 +0200

    ajout anim load temporaire

--- programme.js
+++ programme.js
@@ -1,13 +1,12 @@
-const countainer_prog_jours = document.querySelector(".container_jours");
-const countainer_prog_jour = document.querySelector(".container_jour");
-const jour17 = document.querySelector(".prog_17");
-const jour18 = document.querySelector(".prog_18");
+const container_prog_jours = document.querySelector(".container_jours");
+const container_prog_jour = document.querySelector(".container_jour");
 const select_jour18 = document.querySelector(".select18");
 const select_jour17 = document.querySelector(".select17");
 let artistes_donnees = "";
 const URL = "/programme.json";
 let jour_selected = 17;
 
+//TODO: CHANGER ANIMATION (faire pour chaque image) -> changer structure : ne pas effacer tout le contenu mais uniquement les images.
 
 const repartition = () => {
   let images = document.querySelectorAll(".img_artiste_prog");
@@ -39,39 +38,52 @@ const repartition = () => {
   }
 };
 
-
+const handleLoadImg = () => {
+  container_prog_jour.classList.toggle("invisible");
+  setTimeout(() => {
+    container_prog_jour.classList.toggle("invisible");
+  }, 1500);
+  // let artistes_spin = document.querySelectorAll(".artiste_programme");
+  // artistes_spin.forEach((artiste) => {
+  //   artiste.classList.toggle("artiste_loading");
+  // });
+  // setTimeout(() => {artiste.classList.toggle("artiste_loading");},1000);
+};
 
 const addartistes = () => {
-  countainer_prog_jour.innerHTML="";
-  if (jour_selected == 18) {
-    for (i = 0; i < artistes_donnees.jours[1].artistes.length; i++) {
-      artiste = document.createElement("div");
-      artiste.classList.add("artiste_programme");
-      artiste_img = document.createElement("img");
-      artiste_img.src = artistes_donnees.jours[1].artistes[i].url;
-      countainer_prog_jour.appendChild(artiste);
-      artiste.appendChild(artiste_img);
-      artiste_img.classList.add("img_artiste_prog");
+  handleLoadImg();
+  setTimeout(() => {
+    container_prog_jour.innerHTML = "";
+    if (jour_selected == 18) {
+      for (i = 0; i < artistes_donnees.jours[1].artistes.length; i++) {
+        artiste = document.createElement("div");
+        artiste.classList.add("artiste_programme");
+        artiste_img = document.createElement("img");
+        artiste_img.src = artistes_donnees.jours[1].artistes[i].url;
+        container_prog_jour.appendChild(artiste);
+        artiste.appendChild(artiste_img);
+        artiste_img.classList.add("img_artiste_prog");
+      }
+    } else {
+      for (i = 0; i < artistes_donnees.jours[0].artistes.length; i++) {
+        artiste = document.createElement("div");
+        artiste.classList.add("artiste_programme");
+        artiste_img = document.createElement("img");
+        artiste_img.src = artistes_donnees.jours[0].artistes[i].url;
+        container_prog_jour.appendChild(artiste);
+        artiste.appendChild(artiste_img);
+        artiste_img.classList.add("img_artiste_prog");
+      }
     }
-  } else {
-    for (i = 0; i < artistes_donnees.jours[0].artistes.length; i++) {
-      artiste = document.createElement("div");
-      artiste.classList.add("artiste_programme");
-      artiste_img = document.createElement("img");
-      artiste_img.src = artistes_donnees.jours[0].artistes[i].url;
-      countainer_prog_jour.appendChild(artiste);
-      artiste.appendChild(artiste_img);
-      artiste_img.classList.add("img_artiste_prog");
-    }
-  }
-  repartition();
+    repartition();
+  }, 1000);
 };
 
 const store = (data) => {
   console.log(data.jours);
   artistes_donnees = data;
-  addartistes()
-}
+  addartistes();
+};
 
 const init = async () => {
   let data = await fetch(URL, {}).then((res) => {
@@ -82,29 +94,26 @@ const init = async () => {
   console.log(data);
   store(data);
 
-// const init = async () => {
-//   let data = await fetch(URL, {}).then((res) => {
-//     console.log(res);
-//     return res.json();
-//   });
+  // const init = async () => {
+  //   let data = await fetch(URL, {}).then((res) => {
+  //     console.log(res);
+  //     return res.json();
+  //   });
 
-//   console.log(data);
-//   repartition(data);
-// };
-
+  //   console.log(data);
+  //   repartition(data);
+  // };
 };
 
 init();
 window.addEventListener("resize", repartition);
 
-select_jour17.addEventListener("click",()=>{
+select_jour17.addEventListener("click", () => {
   console.log("coucou");
-  jour_selected=17;
-addartistes();
-
-})
-select_jour18.addEventListener("click",()=>{
-  jour_selected=18;
-addartistes();
-
-})
+  jour_selected = 17;
+  addartistes();
+});
+select_jour18.addEventListener("click", () => {
+  jour_selected = 18;
+  addartistes();
+});
